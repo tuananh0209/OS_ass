ccb44e7c178af0c43cfa33b7931f359bd6f80f05
tuananh0209 <huynhtuananh0209@gmail.com>
2020-04-19 00:05:32 +0700

    fix readline file

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include "findsubstr.h"
 #include "readline.h"
 
-int main (int argc , char *argv[]){
-    printf("asd");
+int main (int argc , char **argv){
+    char line[100];
+    while (read_line(line) > 0){
+        int chk = find_sub_string(line , argv);
+        printf("%d \n", chk);
+    }
+    // printf( "%s", argv[1]);
 }
--- readline.c
+++ readline.c
@@ -5,29 +5,31 @@
 
 #include "readline.h"
 
-int main(int argc , char **argv){
-    char a[100] = "";
-    int i = 0;
-    while(i <4){
-    printf(" \n %d \n",read_line(a));
-    i++;
-    }
-    return 0;
-}
+// int main(int argc , char **argv){
+//     char a[100] = "";
+//     int i = 0;
+//     while(i <4){
+//     printf(" \n %d \n",read_line(a));
+//     i++;
+//     }
+//     return 0;
+// }
 
 int read_line(char *str){
     int p = 0;
     int i = 0;
-    
+    char ch[100] = "";
     do {
         p = fgetc(stdin);
         char a[1] = {(char)p};
         if (p != '\n') {
             strncat(str , a , 1);
+            strncat(ch, a, 1);
         }
        i++;
     }while (p != EOF && p != '\n');
-    printf("%s", str);
+    // printf("%s \n", str);
+    // printf("%s \n", ch);
     if (p == EOF ) return -1;
     else {
         return i - 1;
